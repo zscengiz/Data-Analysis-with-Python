7bd9bd7fd8a8778d84d4af9e7325c6782bb63a13
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2024-01-19 15:11:40 +0300

    Add variables

---
+++ Basics/Variables.py
@@ -0,0 +1,32 @@
+# VARIABLES AND TYPES
+height = 1.79
+weight = 68.7
+bmi = weight / height ** 2
+print(bmi)
+type(bmi) # float
+x = "Sude" # str
+y = True # bool
+print(2 + 3)
+print('ab' + 'ac') # abac
+
+# str()
+savings = 100
+total_savings = 150
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+print("I can add integers, like " + str(5) + " to strings.")
+print("I said " + ("Hey " * 2) + "Hey!")
+
+# Hello World
+print('Hello World')
+
+# Addition
+print(4 + 5)
+
+# Subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+# Division
+print(10 / 2)
