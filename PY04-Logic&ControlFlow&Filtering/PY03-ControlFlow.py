98256ebc24074c23e4ce7ca79e1b95b363599463
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2024-01-20 18:35:54 +0300

    Add control flow

---
+++ PY04-Logic&ControlFlow&Filtering/PY03-ControlFlow.py
@@ -0,0 +1,17 @@
+# example 1
+room = "bed"
+area = 14.0
+
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+
+if area > 15 :
+    print("big place!")
+elif area > 10 : 
+    print("medium size, nice!")
+else :
+    print("pretty small.")
