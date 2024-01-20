04bd01d877d24b256d01abe3b08fc080dd50a888
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2024-01-20 18:35:27 +0300

    Add comparison operators

---
+++ PY04-Logic&ControlFlow&Filtering/PY01-ComparisonOperators.py
@@ -0,0 +1,27 @@
+# Comparison of booleans
+True != False
+True > False
+
+# Comparison of integers
+-5 * 15 != 75
+x = -3 * 6
+x >= -10
+
+# Comparison of strings
+"pyscript" == "PyScript"
+y = "test"
+"test" <= y
+
+# Compare a boolean with an integer
+True == 1
+
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house >= 18)
+
+# my_house less than your_house
+print(my_house <= your_house)
