8f838762b7d38a2a63593f2f9b40db8e3e74aed2
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2024-01-20 16:15:25 +0300

    Add pandas

--- Dictionaries&Pandas/PY03-Pandas.py
+++ Dictionaries&Pandas/PY03-Pandas.py
@@ -1,11 +1,20 @@
+#cars.csv
+#     Unnamed: 0  cars_per_cap        country  drives_right
+#    0         US           809  United States          True
+#    1        AUS           731      Australia         False
+#    2        JPN           588          Japan         False
+#    3         IN            18          India         False
+#    4         RU           200         Russia          True
+#    5        MOR            70        Morocco          True
+#    6         EG            45          Egypt          True
+
 # example 1
+# Import pandas as pd
+import pandas as pd
 # Pre-defined lists
 names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
 dr =  [True, False, False, False, True, True, True]
 cpc = [809, 731, 588, 18, 200, 70, 45]
-
-# Import pandas as pd
-import pandas as pd
 
 # Create dictionary my_dict with three key:value pairs: my_dict
 my_dict = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
@@ -52,3 +61,38 @@ import pandas as pd
 cars = pd.read_csv('cars.csv', index_col=0)
 
 print(cars)
+
+# example 5
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col=0)
+
+# Select the first 3 observations from cars and print them out
+print(cars[0:3])
+
+# Select the fourth, fifth and sixth observation, corresponding to row indexes 3, 4 and 5, and print them out
+print(cars[3:6])
+
+# example 6
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col=0)
+
+# Print out drives_right value of Morocco
+print(cars.loc['MOR', 'drives_right'])
+
+# Print sub-DataFrame for Russia and Morocco, with columns country and drives_right
+print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+
+# example 7
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars.loc[:,'drives_right'])
+#cars.iloc[:, 2]
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:,['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:,['cars_per_cap','drives_right']])
+#cars.iloc[:, [0, 2]]
