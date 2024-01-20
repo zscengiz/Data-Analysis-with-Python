8733d45ec2356d8298c1bcfd30f67ed36e706ab9
Zeynep Sude Cengiz <cengizzsude@gmail.com>
2024-01-20 22:46:47 +0300

    Update pandas

--- PY03-Dictionaries&Pandas/PY03-Pandas.py
+++ PY03-Dictionaries&Pandas/PY03-Pandas.py
@@ -1,13 +1,3 @@
-#cars.csv
-#     Unnamed: 0  cars_per_cap        country  drives_right
-#    0         US           809  United States          True
-#    1        AUS           731      Australia         False
-#    2        JPN           588          Japan         False
-#    3         IN            18          India         False
-#    4         RU           200         Russia          True
-#    5        MOR            70        Morocco          True
-#    6         EG            45          Egypt          True
-
 # example 1
 # Import pandas as pd
 import pandas as pd
